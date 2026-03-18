c4fbc38f2e7e06e04c7256d2554dfe3e0c7205e9
JongWoo Park <jongwoopark@JongWoos-MacBook-Pro.local>
2026-03-18 10:39:12 +0900

    Fix html template for signup
    Password and email input is now properly styled like others

--- src/main/java/com/codesquad/user/User.java
+++ src/main/java/com/codesquad/user/User.java
@@ -3,6 +3,24 @@ package com.codesquad.user;
 public class User {
     private String id;
     private String email;
+    private String password;
+    private String name;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getEmail() {
         return email;
