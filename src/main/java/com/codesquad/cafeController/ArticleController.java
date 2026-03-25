89b67f92d4d8f814de1351f145e62d99d729b634
JongWoo Park <jongwoopark@JongWoos-MacBook-Pro.local>
2026-03-25 12:30:33 +0900

    Implement Log in requirement for qna detail page
    Now User data in Session is required to access detail pages

--- src/main/java/com/codesquad/cafeController/ArticleController.java
+++ src/main/java/com/codesquad/cafeController/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/qna")
@@ -38,9 +39,16 @@ public class ArticleController {
     }
 
     @GetMapping("/{articleId}")
-    public String getArticleDetail(@PathVariable int articleId, Model model){
+    public String getArticleDetail(@PathVariable int articleId, Model model, HttpSession session, RedirectAttributes redirectAttributes){
+
+        if(session.getAttribute("currentUser") == null){
+            redirectAttributes.addFlashAttribute("errorMessage", "Please Log In to Read Questions");
+            return "redirect:/user/login";
+        }
+
         model.addAttribute("article", service.findArticleById(articleId));
         return "qna/questionDetail";
     }
+
 
 }
